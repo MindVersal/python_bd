85e5f9e577c7be7b400c754c2ff2613cb0e66c2d
MindVersal <antajchik@gmail.com>
2017-07-25 14:35:52 +0300

    14:34 25.07.2017

--- python_qt_bd_controller.py
+++ python_qt_bd_controller.py
@@ -57,99 +57,137 @@ def select_from_db(family='', name='', farther='',
                          'КОЗЕРОГ': '23-20',
                          'ВОДОЛЕЙ': '21-19',
                          'РЫБЫ': '20-20'}
+    if zodiak != '' and zodiak != ' ':
+        temp_birthday_month = zodiak_dates_months[zodiak].upper().split(sep='-')
+        temp_birthday_day = zodiak_dates_days[zodiak].upper().split(sep='-')
+        if no_first_where_in_sql:
+            sql_request_where += """ AND """
+        else:
+            no_first_where_in_sql = True
+        sql_request_where += """ (
+                                    (BIRTHDAY_MONTH=\'{0}\' AND
+                                        (BIRTHDAY_DAY BETWEEN \'{1}\' AND \'{2}\')
+                                    )
+                                    OR
+                                    (BIRTHDAY_MONTH=\'{3}\' AND
+                                        (BIRTHDAY_DAY BETWEEN \'{4}\' AND \'{5}\')
+                                    )
+                                )
+                             """. \
+            format(temp_birthday_month[0],  # 0
+                   temp_birthday_day[0],    # 1
+                   '31',                    # 2
+                   temp_birthday_month[1],  # 3
+                   '1',                     # 4
+                   temp_birthday_day[1])    # 5
+
+        birthday_month = ''
+        birthday_day = ''
     if family != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
         if interactive:
-            sql_request_where += """ (FAMILY LIKE '""" + family.upper() + """%') """
+            sql_request_where += """ (FAMILY LIKE \'{0}%\') """.format(family.upper())
         else:
-            sql_request_where += """ (FAMILY='""" + family.upper() + """') """
+            sql_request_where += """ (FAMILY=\'{0}\') """.format(family.upper())
     if name != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
         if interactive:
-            sql_request_where += """ (NAME LIKE '""" + name.upper() + """%')"""
+            sql_request_where += """ (NAME LIKE \'{0}%\')""".format(name.upper())
         else:
-            sql_request_where += """ (NAME='""" + name.upper() + """')"""
+            sql_request_where += """ (NAME=\'{0}\')""".format(name.upper())
     if farther != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
         if interactive:
-            sql_request_where += """ (FARTHER LIKE '""" + farther.upper() + """%')"""
+            sql_request_where += """ (FARTHER LIKE \'{0}%\')""".format(farther.upper())
         else:
-            sql_request_where += """ (FARTHER='""" + farther.upper() + """')"""
+            sql_request_where += """ (FARTHER=\'{0}\')""".format(farther.upper())
     if birthday_year != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
-        sql_request_where += """ (BIRTHDAY='""" + birthday_year.upper() + """')"""
+        if len(birthday_year) == 2:
+            birthday_year = '19' + birthday_year
+        sql_request_where += """ (BIRTHDAY_YEAR=\'{0}\')""".format(birthday_year.upper())
     if birthday_month != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
-        sql_request_where += """ (BIRTHDAY_MONTH='""" + birthday_month.upper() + """')"""
+        birthday_month_list = birthday_month.replace(' ', '').split(sep='-')
+        if len(birthday_month_list) == 1:
+            sql_request_where += """ (BIRTHDAY_MONTH=\'{0}\')""".format(birthday_month.upper())
+        elif len(birthday_month_list) == 2:
+            sql_request_where += """ (BIRTHDAY_MONTH BETWEEN \'{0}\' AND \'{1}\')""".\
+                format(birthday_month_list[0].upper(), birthday_month_list[1].upper())
     if birthday_day != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
-        sql_request_where += """ (BIRTHDAY_DAY='""" + birthday_day.upper() + """')"""
+        birthday_day_list = birthday_day.replace(' ', '').split(sep='-')
+        if len(birthday_day_list) == 1:
+            sql_request_where += " (BIRTHDAY_DAY=\'{0}\')".format(birthday_day_list[0].upper())
+        elif len(birthday_day_list) == 2:
+            sql_request_where += """ (BIRTHDAY_DAY BETWEEN \'{0}\' AND \'{1}\')""".\
+                format(birthday_day_list[0].upper(), birthday_day_list[1].upper())
     if ksiva != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
         if interactive:
-            sql_request_where += """ (KSIVA LIKE '%""" + ksiva.upper() + """%')"""
+            sql_request_where += """ (KSIVA LIKE \'%{0}%\')""".format(ksiva.upper())
         else:
-            sql_request_where += """ (KSIVA='""" + ksiva.upper() + """')"""
+            sql_request_where += """ (KSIVA=\'{0}\')""".format(ksiva.upper())
     if city != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
         if interactive:
-            sql_request_where += """ (CITY LIKE '""" + city.upper() + """%')"""
+            sql_request_where += """ (CITY LIKE \'{0}%\')""".format(city.upper())
         else:
-            sql_request_where += """ (CITY='""" + city.upper() + """')"""
+            sql_request_where += """ (CITY=\'{0}\')""".format(city.upper())
     if selsovet != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
         if interactive:
-            sql_request_where += """ (SELSOVET LIKE '%""" + selsovet.upper() + """%')"""
+            sql_request_where += """ (SELSOVET LIKE \'%{0}%\')""".format(selsovet.upper())
         else:
-            sql_request_where += """ (SELSOVET='""" + selsovet.upper() + """')"""
+            sql_request_where += """ (SELSOVET=\'{0}\')""".format(selsovet.upper())
     if street != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
         if interactive:
-            sql_request_where += """ (STREET LIKE '%""" + street.upper() + """%')"""
+            sql_request_where += """ (STREET LIKE \'%{0}%\')""".format(street.upper())
         else:
-            sql_request_where += """ (STREET='""" + street.upper() + """')"""
+            sql_request_where += """ (STREET=\'{0}\')""".format(street.upper())
     if house != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
-        sql_request_where += """ (STREET='""" + house.upper() + """')"""
+        sql_request_where += """ (STREET=\'{0}\')""".format(house.upper())
     if flat != '':
         if no_first_where_in_sql:
             sql_request_where += """ AND """
         else:
             no_first_where_in_sql = True
-        sql_request_where += """ (STREET='""" + flat.upper() + """')"""
+        sql_request_where += """ (STREET=\'{0}\')""".format(flat.upper())
     if no_first_where_in_sql:
         sql_request = sql_request_start + sql_request_where
     else:
